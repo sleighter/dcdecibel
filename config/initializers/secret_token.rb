d8fed88f3be517e049ecff9c6ac48a9dd73f8171
Alex <alex@ubuntu.ubuntu-domain>
2012-01-01 16:53:54 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dcdecibel::Application.config.secret_token = 'a13df2ec66ebea28b2693b8d267fbc4e38617b1383c45e56eb8575afcdb4c59da4cbeb61d0ff7bebf18d8c2cba46570b050a95f16c30f1d0b95b1b58f5b8a219'
