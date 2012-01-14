b027e6145323a5706f38020d48a2b4db563f164d
Alex Sleighter <alex.sleighter@gmail.com>
2012-01-14 16:08:27 -0500

    actually setting host param

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-config.action_mailer.default_url_options = { :host => 'dcdecibel.heroku.com' }
-
 # Initialize the rails application
 Dcdecibel::Application.initialize!
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,8 @@ Dcdecibel::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => 'dcdecibel.heroku.com' }
+
   # Enable threaded mode
   # config.threadsafe!
 
