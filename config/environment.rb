f83b904c3210313b0ec80dbfac54f081bfb24370
Alex Sleighter <alex.sleighter@gmail.com>
2012-01-14 16:05:55 -0500

    setting host param in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+config.action_mailer.default_url_options = { :host => 'dcdecibel.heroku.com' }
+
 # Initialize the rails application
 Dcdecibel::Application.initialize!
