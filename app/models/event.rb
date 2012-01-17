a23d5e17770e9ac48f2df80608d9f8613e696713
Alex Sleighter <alex.sleighter@gmail.com>
2012-01-16 22:59:02 -0500

    lets actually order events by name

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,5 +4,5 @@ class Event < ActiveRecord::Base
   has_many :tickets
   scope :upcoming, where("event_datetime > ?",Time.now)
   scope :between_dates, lambda{ |begin_date,end_date| where("event_datetime >= ?",begin_date) and where("event_datetime <= ?", end_date)}
-  default_scope :order => "event_datetime ASC"
+  default_scope :order => "name ASC"
 end
