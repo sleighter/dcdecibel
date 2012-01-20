a6f031ac6e2360815eeb051b8d4b78d5352ebcb6
Alex Sleighter <alex.sleighter@gmail.com>
2012-01-19 23:31:00 -0500

    Added ajax loading of details row for venue and band. Improved styling to match comp.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,4 +1,46 @@
+var openRow;
+var loadingHolder = '<td colspan="4" class="grid_16 centered"><image src="/assets/loading.gif" alt="loading" /></td>';
 $(document).ready(function(){
   $('th').addClass('grid_4');
   $('td').addClass('grid_4');
+  
+  $('.band-link').live('click',function(){
+    if ($(this)[0] == openRow)
+    {
+      $('#details-row').slideUp('fast','swing');
+      openRow = null;
+      return;
+    }
+    openRow = $(this)[0];
+    var thisRow = $(this).parent('tr');
+    var bandid = $(this).attr('data-bandid');
+    $('#details-row').slideUp('fast','swing',function() {
+      $('#details-row').html(loadingHolder);
+      $('#details-row').insertAfter(thisRow);
+      $('#details-row').slideDown('fast','swing');
+    });
+    $.get("/bands/" + bandid.toString() + ".json", function(data){ 
+      $('#details-row').html('<td colspan="4" class="grid_16 centered">' + data.name + '  <a href="' + data.homepage_url + '">Homepage</a>' + '</td>'); } 
+    );
+  });
+  
+  $('.venue-link').live('click',function(){
+    if ($(this)[0] == openRow)
+    {
+      $("#details-row").slideUp('fast','swing');
+      openRow = null;
+      return;
+    }
+    openRow = $(this)[0];
+    var thisRow = $(this).parent('tr');
+    var venueid = $(this).attr('data-venueid');
+    $('#details-row').slideUp('fast','swing',function() {
+      $('#details-row').html(loadingHolder);
+      $('#details-row').insertAfter(thisRow);
+      $('#details-row').slideDown('fast','swing');
+    });
+    $.get("/venues/" + venueid.toString() + ".json",function(data){ 
+      $('#details-row').html('<td colspan="4" class="grid_16 centered">' + data.name + '  <a href="' + data.homepage_url + '">Homepage</a>' + '</td>'); } 
+    );
+  });
 });
