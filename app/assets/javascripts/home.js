036ac03ecadfa16e9e4b86407400698bb791d6d3
Alex Sleighter <alex.sleighter@gmail.com>
2012-02-11 15:10:48 -0500

    fixing chrome FF data loading bug

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -90,7 +90,8 @@ $(document).ready(function(){
         if(data.last_fm_id){
           $.get(lastFmUrl(data.last_fm_id),
           function(data2){
-              data2 = JSON.parse(data2);
+              if(!data2.artist)
+                data2 = JSON.parse(data2);
               if (data2.artist){
                 data.image_url = data2.artist.image[3]['#text'];
               }
