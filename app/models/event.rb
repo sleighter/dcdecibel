3d57bcf4c734b20090d44a712760e852491f85cd
Alex Sleighter <alex.sleighter@gmail.com>
2012-02-28 20:31:03 -0500

    Understanding that time will no longer be included on events, so we base our inclusion on date only.

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,9 +3,9 @@ class Event < ActiveRecord::Base
   belongs_to :band, :foreign_key => "headline_band_id"
   belongs_to :venue
   has_many :tickets
-  scope :upcoming, where("event_datetime > ?",Time.now.utc - 5.hours)
-  scope :just_announced, where("created_at > ? AND event_datetime > ?",7.days.ago.to_datetime,Time.now.utc - 5.hours)
-  scope :tonight, where("event_datetime > ? AND event_datetime < ?",Time.now.utc - 5.hours, Time.now.utc.end_of_day - 5.hours)
+  scope :upcoming, where("event_datetime > ?",Time.now.beginning_of_day - 5.hours)
+  scope :just_announced, where("created_at > ? AND event_datetime > ?",7.days.ago.to_datetime,Time.now.utc.beginning_of_day - 5.hours)
+  scope :tonight, where("event_datetime > ? AND event_datetime < ?",Time.now.utc.beginning_of_day - 5.hours, Time.now.utc.end_of_day - 5.hours)
   scope :between_dates, lambda{ |begin_date,end_date| where("event_datetime >= ?",begin_date) and where("event_datetime <= ?", end_date)}
 
   attr_accessor :bandid, :title, :is_in_presale
