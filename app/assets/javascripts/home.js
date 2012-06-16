76757084a70c64b1854d935067955d0e873ce142
Alex Sleighter <alex.sleighter@gmail.com>
2012-06-15 23:01:43 -0400

    fixing bug in info panel cookie

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -208,7 +208,7 @@ $(document).ready(function(){
     closeInfoPanel();
   });
 
-  if(!$.cookie('close_info_panel')){
+  if(!($.cookie('close_info_panel') == 'true')) {
     openInfoPanel();
   }
 });
