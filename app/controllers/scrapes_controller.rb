4f47892e34291222efde164348445a734294943a
Alex Sleighter <alex.sleighter@gmail.com>
2012-06-19 21:18:00 -0400

    improved blackcat scrape to use regex for time and price

--- app/controllers/scrapes_controller.rb
+++ app/controllers/scrapes_controller.rb
@@ -21,10 +21,21 @@ class ScrapesController < ApplicationController
     @events = Array.new
     @new_events = Array.new
     page.search(".show-details").each do |details|
+      price_and_time = details.search(".show-text").text
       event = Event.new
       event.title = details.search(".headline a").text
-      event.event_datetime = Time.parse(details.search(".date").text)
+      begin
+        time = /[0-9]{1,2}[:][0-9]{2}/.match(price_and_time)[0]
+        event.event_datetime = Time.parse(details.search(".date").text + " " + time + "PM")
+      rescue
+      end
       event.opening_bands = Array.new
+      price = /[$][0-9]{1,3}/.match(price_and_time)
+      begin  
+        event.price_min = price[0].sub("$","")
+        event.price_max = price[1].sub("$")
+      rescue
+      end
       details.search(".support").each do |support|
         event.opening_bands.push(support.text)
       end
