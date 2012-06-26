28b177ff3950f2fe844987447361f2778bc90d35
Alex Sleighter <alex.sleighter@gmail.com>
2012-06-25 21:01:26 -0400

    Fixing the admin page to exclude lacking scrape venues

--- app/admin/venues.rb
+++ app/admin/venues.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Venue do
       (venue.homepage_url != nil and venue.homepage_url != "") ? link_to("Homepage", venue.homepage_url) : ""
     end
     column "Scrape Link", :scrape_url do |venue|
-      (venue.scrape_url != nil) ? link_to("Scrape It!", "/scrapes/" + venue.id.to_s) : ""
+      (venue.scrape_url != nil and venue.scrape_class != nil and venue.scrape_url != "" and venue.scrape_class != "") ? link_to("Scrape It!", "/scrapes/" + venue.id.to_s) : ""
     end
     column "Phone", :phone
     column "Editor\'s Choice?", :is_editors_choice do |band|
